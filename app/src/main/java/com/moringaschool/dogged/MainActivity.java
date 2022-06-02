82db7ca4c7d12cf14267a0be53c82e99b87efb57
jackie <bobbinnej@gmail.com>
2022-06-02 13:40:57 +0300

    Make splashscreen api become the launcher activity, increase window splashscreen animation duration in themes.xml

--- app/src/main/java/com/moringaschool/dogged/MainActivity.java
+++ app/src/main/java/com/moringaschool/dogged/MainActivity.java
@@ -3,12 +3,14 @@ package com.moringaschool.dogged;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SplashScreen.installSplashScreen(this);
         setContentView(R.layout.activity_main);
     }
 }

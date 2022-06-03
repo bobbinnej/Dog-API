ab9c61721f28d57f71f245bfd1cefdc031899c6c
jackie <bobbinnej@gmail.com>
2022-06-03 13:26:18 +0300

    Define the structure for my ListAllBreedsResponse call in DogApi interface

---
+++ app/src/main/java/com/moringaschool/dogged/interfaces/DogApi.java
@@ -0,0 +1,18 @@
+package com.moringaschool.dogged.interfaces;
+
+import androidx.cardview.widget.CardView;
+
+import com.moringaschool.dogged.models.ListAllBreedsResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface DogApi {
+    //Define your calls
+    //our method is get
+    @GET("breeds/list/all") //this is the endpoint for the list of all breeds
+    //this our API call and wat is inside the angle brackets is the expected response which is the ListAllBreedsResponse
+    Call<ListAllBreedsResponse> getAllBreeds();  //getAllBreeds is our method which can be any name you want.
+
+
+}

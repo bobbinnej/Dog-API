82f85b2bad8d868bfb0fa15a3519ce97b28de8a2
jackie <bobbinnej@gmail.com>
2022-06-04 16:48:45 +0300

    Create the toolbar for tabbed layout

--- app/src/main/java/com/moringaschool/dogged/MainActivity.java
+++ app/src/main/java/com/moringaschool/dogged/MainActivity.java
@@ -3,14 +3,24 @@ package com.moringaschool.dogged;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.splashscreen.SplashScreen;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SplashScreen.installSplashScreen(this);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+
     }
 }
--- app/src/main/java/com/moringaschool/dogged/interfaces/DogApi.java
+++ app/src/main/java/com/moringaschool/dogged/interfaces/DogApi.java
@@ -9,7 +9,7 @@ import retrofit2.http.GET;
 
 public interface DogApi {
     //Define your calls
-    //our method is get
+    //our method for getting the list of all breeds is get
     @GET("breeds/list/all") //this is the endpoint for the list of all breeds
     //this our API call and wat is inside the angle brackets is the expected response which is the ListAllBreedsResponse
     Call<ListAllBreedsResponse> getAllBreeds();  //getAllBreeds is our method which can be any name you want.

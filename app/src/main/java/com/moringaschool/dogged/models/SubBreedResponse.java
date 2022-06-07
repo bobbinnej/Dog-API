e905d81b5d656b06161b48bd1a859d88f24e4734
jackie <bobbinnej@gmail.com>
2022-06-07 11:36:08 +0300

    generate pojo for subbreed response class

--- app/src/main/java/com/moringaschool/dogged/models/SubBreedResponse.java
+++ app/src/main/java/com/moringaschool/dogged/models/SubBreedResponse.java
@@ -1,4 +1,51 @@
 package com.moringaschool.dogged.models;
 
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class SubBreedResponse {
+
+    @SerializedName("message")
+    @Expose
+    private List<String> message = null;
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public SubBreedResponse() {
+    }
+
+    /**
+     *
+     * @param message
+     * @param status
+     */
+    public SubBreedResponse(List<String> message, String status) {
+        super();
+        this.message = message;
+        this.status = status;
+    }
+
+    public List<String> getMessage() {
+        return message;
+    }
+
+    public void setMessage(List<String> message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }

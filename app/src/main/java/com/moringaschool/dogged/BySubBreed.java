cb25bdb7f5331851abbf911a6b9fbaea9b532b91
jackie <bobbinnej@gmail.com>
2022-06-07 12:09:07 +0300

    Establish connection between api and subbreed fragment class

--- app/src/main/java/com/moringaschool/dogged/BySubBreed.java
+++ app/src/main/java/com/moringaschool/dogged/BySubBreed.java
@@ -1,58 +1,51 @@
 package com.moringaschool.dogged;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BySubBreed#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.moringaschool.dogged.RetrofitClient.DogClient;
+import com.moringaschool.dogged.interfaces.DogApi;
+import com.moringaschool.dogged.models.SubBreedResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class BySubBreed extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public BySubBreed() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BySubBreed.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BySubBreed newInstance(String param1, String param2) {
-        BySubBreed fragment = new BySubBreed();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    DogApi dogApi;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dogApi= DogClient.getClient();
+        getSubBreed();
+    }
+
+    public void getSubBreed(){
+        Call<SubBreedResponse>call= dogApi.getSubBreed();
+        call.enqueue(new Callback<SubBreedResponse>() {
+            @Override
+            public void onResponse(Call<SubBreedResponse> call, Response<SubBreedResponse> response) {
+                int status=response.code();
+                SubBreedResponse subBreedResponse=response.body();
+            }
+
+            @Override
+            public void onFailure(Call<SubBreedResponse> call, Throwable t) {
+                Log.e(TAG,"OOOOOOPERATION  get sub Breed FAILED"+t.getMessage());
+            }
+        });
     }
 
     @Override

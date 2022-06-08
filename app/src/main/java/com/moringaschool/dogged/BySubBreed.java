d7f04925eac11620c5ee6550ba1a1a3f5441811a
jackie <bobbinnej@gmail.com>
2022-06-08 17:15:58 +0300

    edit image display in randombreed

--- app/src/main/java/com/moringaschool/dogged/BySubBreed.java
+++ app/src/main/java/com/moringaschool/dogged/BySubBreed.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,18 +19,30 @@ import com.moringaschool.dogged.RetrofitClient.DogClient;
 import com.moringaschool.dogged.interfaces.DogApi;
 import com.moringaschool.dogged.models.SubBreedResponse;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class BySubBreed extends Fragment {
     DogApi dogApi;
+    private List<SubBreedResponse> subbreed;
+    LinearLayoutManager linearLayoutManager;
+
+    // recycler view
+    @BindView(R.id.subbreedRecycler)  RecyclerView subRecycler;
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this,view);
 
         dogApi= DogClient.getClient();
+        subRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
         getSubBreed();
     }
 
@@ -39,6 +53,9 @@ public class BySubBreed extends Fragment {
             public void onResponse(Call<SubBreedResponse> call, Response<SubBreedResponse> response) {
                 int status=response.code();
                 SubBreedResponse subBreedResponse=response.body();
+
+                List<String>list=subBreedResponse.getMessage();
+                subRecycler.setAdapter(new SubbreedAdapter(list, getContext()));
             }
 
             @Override
